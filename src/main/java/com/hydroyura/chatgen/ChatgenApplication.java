171db91637ebf73d32d2bce9d1a93550bf43276a
yury.klimchuk <hydro.yura@gmail.com>
2026-05-20 22:22:10 +0300

    init commit

---
+++ src/main/java/com/hydroyura/chatgen/ChatgenApplication.java
@@ -0,0 +1,13 @@
+package com.hydroyura.chatgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgenApplication.class, args);
+	}
+
+}
